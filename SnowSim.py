fc06399b6d0a5078e7a88d2968e4e51bdb3c384c
Hongda Zeng <hongda.code@gmail.com>
2014-11-22 15:34:52 -0500

    display and change messages

--- SnowSim.py
+++ SnowSim.py
@@ -21,6 +21,10 @@ SnowSize = 5
 DY = 1
 DX = 1
 
+myfont = pygame.font.SysFont("monospace", 150)
+label1 = myfont.render("Merry", 1, (204, 22, 0))
+label2 = myfont.render("Christmas!", 1, (204, 22, 0))
+
 screen = pygame.display.set_mode((MyScreenLength, MyScreenWidth), pygame.FULLSCREEN)
 pygame.display.set_caption("Snow Animation")
 
@@ -29,13 +33,14 @@ help_image = pygame.image.load("h2.png").convert()
 
 pygame.mixer.music.load('silent.ogg')
 pygame.mixer.music.play(0)
+
 # Create an empty array
-snow_list = []
+Snow_List = []
 
 for i in range(500):
     x = random.randrange(0, MyScreenLength)
     y = random.randrange(0, MyScreenWidth)
-    snow_list.append([x, y])
+    Snow_List.append([x, y])
 
 clock = pygame.time.Clock()
 
@@ -53,11 +58,11 @@ while not done:
                 for b in range(10):
                     x = random.randrange(0, MyScreenLength)
                     y = random.randrange(0, MyScreenWidth)
-                    snow_list.append([x, y])
+                    Snow_List.append([x, y])
             elif pygame.key.get_pressed()[pygame.K_DOWN]:
-                if len(snow_list) > 20:
+                if len(Snow_List) > 20:
                     for c in range(10):
-                        snow_list.pop(random.randrange(len(snow_list)))
+                        Snow_List.pop(random.randrange(len(Snow_List)))
             elif pygame.key.get_pressed()[pygame.K_LEFT]:
                 if SnowSize > 1:
                     SnowSize -= 1
@@ -83,56 +88,71 @@ while not done:
                 DY = 1
             elif pygame.key.get_pressed()[pygame.K_m]:
                 if PlayingMusic:
-                    pygame.mixer.pause()
+                    pygame.mixer.music.pause()
                     PlayingMusic = False
 
                 else:
-                    pygame.mixer.unpause()
+                    pygame.mixer.music.unpause()
                     PlayingMusic = True
-
+            elif pygame.key.get_pressed()[pygame.K_RETURN]:
+                d = random.randint(1, 5)
+                if d == 1:
+                    label1 = myfont.render("Merry", 1, (204, 22, 0))
+                    label2 = myfont.render("Christmas!", 1, (204, 22, 0))
+                if d == 2:
+                    label1 = myfont.render("Happy", 1, (204, 22, 0))
+                    label2 = myfont.render("New Year!", 1, (204, 22, 0))
+                if d == 3:
+                    label1 = myfont.render("Party", 1, (204, 22, 0))
+                    label2 = myfont.render("Hard!", 1, (204, 22, 0))
+                if d == 4:
+                    label1 = myfont.render("Code", 1, (204, 22, 0))
+                    label2 = myfont.render("On!", 1, (204, 22, 0))
 
     screen.blit(background_image, [0, 0])
     screen.blit(help_image, [1500, 50])
 
     # Process each snow flake in the list
-    for i in range(len(snow_list)):
+    for i in range(len(Snow_List)):
 
         # Draw the snow flake
-        pygame.draw.circle(screen, WHITE, snow_list[i], SnowSize)
+        pygame.draw.circle(screen, WHITE, Snow_List[i], SnowSize)
 
         # Move the snow flake based on dx and dy
-        snow_list[i][0] += DX
-        snow_list[i][1] += DY
+        Snow_List[i][0] += DX
+        Snow_List[i][1] += DY
 
         # reset the snow if it completely left the screen
-        if snow_list[i][1] > MyScreenWidth + SnowSize:
+        if Snow_List[i][1] > MyScreenWidth + SnowSize:
             # Reset it some distance above the top
-            y = random.randrange(-1*SnowSize, 0*SnowSize)
-            snow_list[i][1] = y
+            y = random.randrange(-1 * SnowSize, 0 * SnowSize)
+            Snow_List[i][1] = y
             # Give it a new x position
             x = random.randrange(0, MyScreenLength)
-            snow_list[i][0] = x
+            Snow_List[i][0] = x
 
-        if snow_list[i][0] > MyScreenLength + SnowSize:
+        if Snow_List[i][0] > MyScreenLength + SnowSize:
             # Reset it some distance from left
-            x = random.randrange(-1*SnowSize, 0*SnowSize)
-            snow_list[i][0] = x
+            x = random.randrange(-1 * SnowSize, 0 * SnowSize)
+            Snow_List[i][0] = x
             # Give it a new x position
             y = random.randrange(0, MyScreenWidth)
-            snow_list[i][1] = y
+            Snow_List[i][1] = y
 
-        if snow_list[i][0] < 0 - SnowSize - 5:
-            x = random.randrange(-10*SnowSize + MyScreenLength, -2*SnowSize + MyScreenLength)
+        if Snow_List[i][0] < 0 - SnowSize - 5:
+            x = random.randrange(-10 * SnowSize + MyScreenLength, -2 * SnowSize + MyScreenLength)
             y = random.randrange(0, MyScreenWidth)
-            snow_list[i][0] = x
-            snow_list[i][1] = y
+            Snow_List[i][0] = x
+            Snow_List[i][1] = y
 
-        if snow_list[i][1] < 0 - SnowSize - 5:
-            y = random.randrange(-10*SnowSize + MyScreenWidth, -2*SnowSize + MyScreenWidth)
+        if Snow_List[i][1] < 0 - SnowSize - 5:
+            y = random.randrange(-10 * SnowSize + MyScreenWidth, -2 * SnowSize + MyScreenWidth)
             x = random.randrange(0, MyScreenLength)
-            snow_list[i][0] = x
-            snow_list[i][1] = y
+            Snow_List[i][0] = x
+            Snow_List[i][1] = y
 
+    screen.blit(label1, (100, 100))
+    screen.blit(label2, (250, 250))
     # Yo update
     pygame.display.flip()
     clock.tick(60)
